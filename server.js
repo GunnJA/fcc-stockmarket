f2b83c7f62bee1c8f2969620ae3954982db17a87
Jonathan Gunn <gunnja87@gmail.com>
2018-04-09 09:03:44 +0000

    another heroku fix 2

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static(__dirname + '/client'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendfile('index.html');
+  res.sendfile('./index.html');
 });
 
 // listen for requests :)
